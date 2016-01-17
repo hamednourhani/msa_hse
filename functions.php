600654b4796229b20a056d70c67c52e9aa442427
Hamed Nourhani <info@itstar.ir>
2016-01-17 11:24:17 +0330

    side menu added

--- functions.php
+++ functions.php
@@ -5,6 +5,7 @@ do_action('icl_language_selector');
 }
 add_shortcode( 'wpml_lang_selector', 'wpml_shortcode_func' );
 
+
 /*
 default anguage selector
  <div class="lang-sel sel-dropdown"><a href="#"><span>En</span></a><ul><li><a href="#">En</a></li><li><a href="#">De</a></li><li><a href="#">Fr</a></li></ul></div>
@@ -13,3 +14,19 @@ header top addresss bar default
 <i class="dfd-icon-phone"></i><span class="dfd-top-info-delim-blank"></span>+1234567890<span class="dfd-top-info-delim"></span><i class="dfd-icon-email_2"></i><span class="dfd-top-info-delim-blank"></span>info@yourmail.com
 
  */
+
+if ( ICL_LANGUAGE_CODE=='en'){ 
+  
+        remove_filter('the_title', 'ztjalali_persian_num');
+        remove_filter('the_content', 'ztjalali_persian_num');
+        remove_filter('the_excerpt', 'ztjalali_persian_num');
+        remove_filter('comment_text', 'ztjalali_persian_num');
+    // change arabic characters
+        remove_filter('the_content', 'ztjalali_ch_arabic_to_persian');
+        remove_filter('the_title', 'ztjalali_ch_arabic_to_persian');
+        remove_filter('the_excerpt', 'ztjalali_ch_arabic_to_persian');
+        remove_filter('comment_text', 'ztjalali_ch_arabic_to_persian');
+    
+
+
+}
