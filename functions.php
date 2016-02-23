c1c20af34604182419a7064babf9a7cec785e026
Hamed Nourhani <info@itstar.ir>
2016-02-23 18:18:28 +0330

    first commit

--- functions.php
+++ functions.php
@@ -4,8 +4,11 @@ function wpml_shortcode_func(){
 do_action('icl_language_selector');
 }
 add_shortcode( 'wpml_lang_selector', 'wpml_shortcode_func' );
-
-
+function msa_scripts_and_styles()
+{
+        wp_register_script( 'hermooder-modernizr', get_stylesheet_directory_uri() . '/js/lib/modernizr.custom.min.js', array(), '2.5.3', false );
+}
+}
 /*
 default anguage selector
  <div class="lang-sel sel-dropdown"><a href="#"><span>En</span></a><ul><li><a href="#">En</a></li><li><a href="#">De</a></li><li><a href="#">Fr</a></li></ul></div>
